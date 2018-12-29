b006841794741308df34b60632f0f40e6e63fadb
sairakaye <30943505+sairakaye@users.noreply.github.com>
2018-12-29 16:49:15 +0800

    Organized the folders.

--- WEBAPDE-MP1-Site/js/conres.js
+++
@@ -1,8 +0,0 @@
-$("#confbtn").on('click', function(){
-    alert("Your reservation has been confirmed.");
-    window.location.href="../html/home.html"
-})
-
-$("#prevbtn").on('click', function(){
-    window.location.href="../html/reserve.html"
-})
--- WEBAPDE-MP1-Site/js/start.js
+++
@@ -1,8 +0,0 @@
- $('#login').on('click', function(e) {
-     e.preventDefault()
-     var email = $('#email-field').val()
-     if (email != 'admin@dlsu.edu.ph')
-        window.location.href="../html/home.html"
-     else
-         window.location.href="../html/admin.html"
- })
